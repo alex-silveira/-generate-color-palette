78a8db6852dfc54193fab4c07e644808adbb3564
alex <alexarauj0015@hotmail.com>
2020-06-27 15:58:12 -0300

    Update identation

--- js/generate.js
+++ js/generate.js
@@ -10,8 +10,8 @@ $(function () {
 	$("body").keyup(function(e) {
 		// Check key press 37 = left, 38 = top, 39 = right, 40 = bottom
 		if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){
-		// Generate colors key up
-		generatePalettes();
+			// Generate colors key up
+			generatePalettes();
 		}
 	});
 	
@@ -20,10 +20,10 @@ $(function () {
 		for(let i = 0; i <= 12; i++) {
 			//hexadecimal color code
 			for (let j = 0; j <= 3; j++) {
-			//Generate random hexadecimal letters
-			randomLetters = Math.floor(Math.random() * letters.length);
-			//Generate random hexadecimal numbers
-			randomNumbers = Math.floor(Math.random() * numbers.length).toString();
+				//Generate random hexadecimal letters
+				randomLetters = Math.floor(Math.random() * letters.length);
+				//Generate random hexadecimal numbers
+				randomNumbers = Math.floor(Math.random() * numbers.length).toString();
 
 			if(palette == "palette01"){
 				//Add Hexadecimal color code in array hx palette 1
